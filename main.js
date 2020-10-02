8657fa74252ee2ccdcb2dc1c3f4fde7f9d95a1a3
bhawna chopra <55947219+bhawnachopra2002@users.noreply.github.com>
2020-10-02 12:21:52 +0530

    Update main.js
    added empty functions to call

---
+++ main.js
@@ -0,0 +1,20 @@
+function MergeSort(){
+    console.log("mergesort");
+    return null;
+}
+function HeapSort(){
+    console.log("heapsort");
+    return null;
+}
+function QuickSort(){
+    console.log("quicksort");
+    return null;
+}
+function BubbleSort(){
+    console.log("bubblesort");
+    return null;
+}
+function GenNewArray(){
+    console.log("new array generated");
+    return null;
+}
